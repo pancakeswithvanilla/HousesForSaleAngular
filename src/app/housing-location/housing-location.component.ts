db0ce9e5c6450cbbfeb2ee972923406eade5d7e7
pancakeswithvanilla <ana.poput@yahoo.com>
2025-01-28 23:09:27 +0100

    Initial commit: Set up Angular project for HousesForSale

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: `
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+@Input() housingLocation!: HousingLocation;
+}
